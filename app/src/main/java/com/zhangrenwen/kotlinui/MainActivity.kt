991ab5a631c20af6f033dde7ba7a0f9ccd35ed84
GoodWen <479696877@qq.com>
2020-12-10 10:35:16 +0800

    KotlinUI

---
+++ app/src/main/java/com/zhangrenwen/kotlinui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.zhangrenwen.kotlinui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun progressBarClick(view: View) {
+
+    }
+}
