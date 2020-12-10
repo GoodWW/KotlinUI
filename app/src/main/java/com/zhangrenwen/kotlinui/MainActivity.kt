1553d374aecd8cbb89c43d7c6ea8f90e799ccb96
GoodWen <479696877@qq.com>
2020-12-10 11:27:57 +0800

    AlertDialog   对话框

--- app/src/main/java/com/zhangrenwen/kotlinui/MainActivity.kt
+++ app/src/main/java/com/zhangrenwen/kotlinui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,18 @@ class MainActivity : BaseActivity() {
 
     fun progressBarClick(view: View) {
         startActivity(Intent(this, ProgressBarActivity::class.java))
+    }
+
+    fun alertDialogClick(view: View) {
+        AlertDialog.Builder(this).apply {
+            setTitle("这是消息标题")
+            setMessage("消息内容")
+            setCancelable(false)//设置是否可以取消
+            setPositiveButton("确定") { dialog, which ->
+                Toast.makeText(this@MainActivity, "确定", Toast.LENGTH_SHORT).show()
+            }
+            setNegativeButton("取消") { dialog, which ->
+            }
+        }.show()
     }
 }
